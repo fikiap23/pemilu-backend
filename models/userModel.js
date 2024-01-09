435c6499c9a68416777d4a8ce5d5661f376ac7c0
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-09 22:08:24 +0700

    add model

--- models/userModel.js
+++ models/userModel.js
@@ -2,28 +2,16 @@ import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
     username: {
       type: String,
       required: true,
       unique: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+    village: {},
     password: {
       type: String,
       minLength: 6,
       required: true,
-    },
-    profilePic: {
-      type: String,
-      default: '',
     },
   },
   {

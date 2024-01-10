b51d90567feda6bb7a041c02f63fb7d368c51102
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-10 09:13:36 +0700

    add login logut user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,9 @@
 import express from 'express'
-import adminController from '../controllers/adminController.js'
-import protectRouteAdmin from '../middlewares/protectRouteAdmin.js'
+import userController from '../controllers/userController.js'
 
 const router = express.Router()
 
-router.post('/signup', adminController.signupUser)
-router.post('/login', adminController.loginUser)
-router.post('/logout', adminController.logoutUser)
-router.post(
-  '/createVillage',
-  protectRouteAdmin,
-  adminController.createNewVillage
-)
-router.post('/createParty', protectRouteAdmin, adminController.createNewParty)
+router.post('/login', userController.loginUser)
+router.post('/logout', userController.logoutUser)
 
 export default router

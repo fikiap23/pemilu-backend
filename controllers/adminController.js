890de5545e3ab1b359e94d4077923c574c573e3f
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-10 16:55:37 +0700

    create structur

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 import Admin from '../models/adminModel.js'
 import User from '../models/userModel.js'
-import Village from '../models/villageModel.js'
+import { District, Regency, Village } from '../models/regionModel.js'
 import Party from '../models/partyModel.js'
 import bcrypt from 'bcryptjs'
 import generateTokenAndSetCookie from '../utils/helpers/generateTokenAndSetCookie.js'
@@ -85,9 +85,22 @@ const adminController = {
       console.log('Error in logout: ', err.message)
     }
   },
+
+  //* kelola user
   createNewUser: async (req, res) => {
     try {
       const { username, password, villageId } = req.body
+
+      // Validate input not null
+      if (!username || !password || !villageId) {
+        return res.status(400).json({ error: 'All fields are required' })
+      }
+
+      // check is village exists
+      const village = await Village.findById(villageId)
+      if (!village) {
+        return res.status(400).json({ error: 'Village not found' })
+      }
 
       // Check if the user already exists
       const existingUser = await User.findOne({ username })
@@ -118,42 +131,7 @@ const adminController = {
     }
   },
 
-  createNewVillage: async (req, res) => {
-    try {
-      let { village_name, total_voters, disterict } = req.body
-
-      // Validate input
-      if (!village_name || !total_voters || !disterict) {
-        return res.status(400).json({ error: 'Missing required fields' })
-      }
-      village_name = village_name.toLowerCase()
-
-      // Check if the village already exists
-      const existingVillage = await Village.findOne({ village_name })
-      if (existingVillage) {
-        return res.status(400).json({ error: 'Village already exists' })
-      }
-
-      // Create new village
-      const newVillage = new Village({
-        disterict,
-        village_name,
-        total_voters,
-      })
-      await newVillage.save()
-
-      res.status(201).json({
-        _id: newVillage._id,
-        disterict: newVillage.disterict,
-        village_name: newVillage.village_name,
-        total_voters: newVillage.total_voters,
-      })
-    } catch (error) {
-      res.status(500).json({ error: error.message })
-      console.log('Error in createNewVillage: ', error.message)
-    }
-  },
-
+  //* kelola partai
   createNewParty: async (req, res) => {
     try {
       let { name, code, path } = req.body
@@ -188,6 +166,109 @@ const adminController = {
     } catch (error) {
       res.status(500).json({ error: error.message })
       console.log('Error in createNewParty: ', error.message)
+    }
+  },
+
+  //* kelola desa
+  createNewVillage: async (req, res) => {
+    try {
+      const { village_name, total_voters, district_id } = req.body
+
+      // Check if input is null
+      if (!village_name || !total_voters || !district_id) {
+        return res.status(400).json({ error: 'Missing required fields' })
+      }
+
+      // check if district exists
+      const district = await District.findById(district_id)
+      if (!district) {
+        return res.status(400).json({ error: 'District not found' })
+      }
+
+      const newVillage = new Village({
+        village_name,
+        district_id,
+        total_voters,
+      })
+
+      await newVillage.save()
+
+      // add to district
+      district.villages.push(newVillage._id)
+      await district.save()
+
+      res.status(201).json({
+        _id: newVillage._id,
+        district_id: newVillage.district_id,
+        village_name: newVillage.village_name,
+      })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+      console.log('Error in createNewVillage: ', error.message)
+    }
+  },
+
+  //* kelola kecamatan
+  createNewDistrict: async (req, res) => {
+    try {
+      const { district_name, regency_id } = req.body
+
+      // Check if input is null
+      if (!district_name || !regency_id) {
+        return res.status(400).json({ error: 'Missing required fields' })
+      }
+
+      // check if regency exists
+      const regency = await Regency.findById(regency_id)
+      if (!regency) {
+        return res.status(400).json({ error: 'Regency not found' })
+      }
+
+      const newDistrict = new District({
+        district_name,
+        regency_id,
+      })
+
+      await newDistrict.save()
+
+      // add to regency
+      regency.districts.push(newDistrict._id)
+      await regency.save()
+
+      res.status(201).json({
+        _id: newDistrict._id,
+        district_name: newDistrict.district_name,
+        regency_id: newDistrict.regency_id,
+      })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+      console.log('Error in createNewDistrict: ', error.message)
+    }
+  },
+
+  //*kelola kabupaten
+  createNewRegency: async (req, res) => {
+    try {
+      const { regency_name } = req.body
+
+      // Check if input is null
+      if (!regency_name) {
+        return res.status(400).json({ error: 'Missing required fields' })
+      }
+
+      const newRegency = new Regency({
+        regency_name,
+      })
+
+      await newRegency.save()
+
+      res.status(201).json({
+        _id: newRegency._id,
+        regency_name: newRegency.regency_name,
+      })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+      console.log('Error in createNewRegency: ', error.message)
     }
   },
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,26 @@
-import express from 'express'
-import userController from '../controllers/userController.js'
-import { protectUserRoute } from '../middlewares/protectRoute.js'
+// import express from 'express'
+// import userController from '../controllers/userController.js'
+// import { protectUserRoute } from '../middlewares/protectRoute.js'
 
-const router = express.Router()
+// const router = express.Router()
 
-router.post('/login', userController.loginUser)
-router.post('/logout', userController.logoutUser)
-router.post(
-  '/validBallots',
-  protectUserRoute,
-  userController.addVotesToValidBallots
-)
+// router.post('/login', userController.loginUser)
+// router.post('/logout', userController.logoutUser)
+// router.post(
+//   '/validBallots',
+//   protectUserRoute,
+//   userController.addVotesToValidBallots
+// )
 
-router.put(
-  '/validBallots',
-  protectUserRoute,
-  userController.updateVotesToValidBallots
-)
-router.patch(
-  '/invalidBallots',
-  protectUserRoute,
-  userController.addVoteToInvalidBallots
-)
+// router.put(
+//   '/validBallots',
+//   protectUserRoute,
+//   userController.updateVotesToValidBallots
+// )
+// router.patch(
+//   '/invalidBallots',
+//   protectUserRoute,
+//   userController.addVoteToInvalidBallots
+// )
 
-export default router
+// export default router
